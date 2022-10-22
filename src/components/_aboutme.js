7129ea0399df1e751a6cf75e500cb119bad11fbc
cdsherrardjr <90732785+cdsherrardjr@users.noreply.github.com>
2022-10-22 13:40:28 -0500

    Edited aboutme page

--- src/components/_aboutme.js
+++ src/components/_aboutme.js
@@ -3,24 +3,30 @@ function AboutMe() {
     <div id="#about-me" className="component">
       <h1>about me</h1>
       <div>
-        <h2>Musician + Artist + Designer + Programmer + Writer</h2>
+        <h2>musician + artist + designer + programmer + writer</h2>
         <p>
           BS, Digital Media from <b>University of Houston</b><br/>
+          Graphic Designer, <b>Minaret Records</b><br/>
           DJ, <b>KTRU 96.1</b><br/>
           Founder, <b>Forum Cache®</b> + <b>reserve®</b> jazz publication<br/>
         </p>
         <p>
-          Art has my life and music has my heart. Exploring both through design, 
-          technology (digital and analog), and language has been my focus since the early 2010s. 
-          I believe that the real work lies in solving problems that impede on the livelihood of 
-          people all over the world. I'm interested in iterating/reiterating new systems for 
-          sustainable living, developing independent, self-sufficient black cultural institutions, 
-          and funding/contributing to decolonization projects.
+          Art has my life and music has my heart. Exploring both through design, digital/analog 
+          technology, and language has been my focus since the mid-2010s. I'm interested in iterating/reiterating 
+          new systems for sustainable living, developing independent, self-sufficient black cultural 
+          institutions, and funding/contributing to decolonization projects. The improvisational nature of 
+          Jazz has influenced the methods in which I approach making. 
         </p>
         <p>
-          I'm always looking for ways to expand my skillset and build my experiences. If there are
-          any projects you'd like to share with me or collaborate on, feel free to contact me through
-          any of the methods available in the footer.
+            Thus far, I've learned front-end web technologies (HTML + CSS + JavaScript + React), industry-standard 
+            design software (Adobe Creative Suite + Figma), and DAWs (Ableton Live + ProTools) I'm currently 
+            learning p5.js to incorporate creative code into my art/design practice. When I'm not building up my toolkit
+            for making things, I play tenor saxophone and trumpet.
+        </p>
+        <p>
+          Thanks for visiting! I'll have an updated resume available here soon, but in the meantime, feel free to email me 
+          at <b>cdsherrardjr@gmail.com</b>, dm me on instagram at <a href="https://www.instagram.com/cds__j" target="_blank" rel="noreferrer"><b>@cds__j</b></a> or
+          message me on linkedin <a href="https://www.linkedin.com/in/corey-d-sherrard-jr-959b6b142/" target="_blank" rel="noreferrer"><b>here</b></a>.
         </p>
       </div>
     </div>

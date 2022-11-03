1b981f93f3089434477946da67c93e7594ccd17b
cdsherrardjr <90732785+cdsherrardjr@users.noreply.github.com>
2022-11-02 19:55:48 -0500

    Updated aboutme

--- src/components/_aboutme.js
+++ src/components/_aboutme.js
@@ -4,11 +4,23 @@ function AboutMe() {
       <h1>about me</h1>
       <div>
         <h2>musician + artist + designer + programmer + writer</h2>
+        <h3>education</h3>
         <p>
-          BS, Digital Media from <b>University of Houston</b><br/>
-          Graphic Designer, <b>Minaret Records</b><br/>
-          DJ, <b>KTRU 96.1</b><br/>
-          Founder, <b>Forum Cache®</b> + <b>reserve®</b> jazz publication<br/>
+          BS, Digital Media from <a href="https://www.uh.edu" target="_blank" rel="noreferrer"><b>University of Houston</b></a><br/>
+          Certification, Front-End Web Development <b>(self-taught, ongoing)</b>
+        </p>
+        <h3>current work</h3>
+        <p>
+          Graphic Designer, <a href="https://www.minaretrecords.com" target="_blank" rel="noreferrer"><b>Minaret Records</b></a><br/>
+          DJ, <a href="https://ktru.org" target="_blank" rel="noreferrer"><b>KTRU 96.1</b></a><br/>
+          Founder, <a href="https://forumcache.xyz" target="_blank" rel="noreferrer"><b>Forum Cache®</b></a> + <b>reserve®</b> jazz publication<br/>
+          Part-Owner + Studio Engineer, <b>Pipeline Engineering</b>
+        </p>
+        <h3>previous experience</h3>
+        <p>
+          2022, Graphic Designer, <a href="https://www.keronfornewyork.com/" target="_blank" rel="noreferrer"><b>KERON FOR NEW YORK</b></a> (Campaign for New York State Assembly District 60)<br/>
+          2017-2020, (First Chair) Studio Engineer, <a href="https://libraries.uh.edu/spaces-tech/multimedia-studio/" target="_blank" rel="noreferrer"><b>Hamill Foundation Multimedia Studio</b></a><br/>
+          2014-2018, Radio Host + Co-host, <a href="https://coogradio.com/" target="_blank" rel="noreferrer"><b>Coog Radio</b></a>
         </p>
         <p>
           Art has my life and music has my heart. Exploring both through design, digital/analog 
@@ -18,10 +30,10 @@ function AboutMe() {
           Jazz has influenced the methods in which I approach making. 
         </p>
         <p>
-            Thus far, I've learned front-end web technologies (HTML + CSS + JavaScript + React), industry-standard 
-            design software (Adobe Creative Suite + Figma), and DAWs (Ableton Live + ProTools) I'm currently 
-            learning p5.js to incorporate creative code into my art/design practice. When I'm not building up my toolkit
-            for making things, I play tenor saxophone and trumpet.
+          Thus far, I've learned front-end web technologies (HTML + CSS + JavaScript + React), industry-standard 
+          design software (Adobe Creative Suite + Figma), and DAWs (Ableton Live + ProTools) I'm currently 
+          learning p5.js to incorporate creative code into my art/design practice. When I'm not building up my toolkit
+          for making things, I play tenor saxophone and trumpet.
         </p>
         <p>
           Thanks for visiting! I'll have an updated resume available here soon, but in the meantime, feel free to email me 

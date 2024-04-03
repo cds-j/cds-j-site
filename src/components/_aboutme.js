5a972d8c0948bfef3c0dabd46f8817a02d300ac2
cds-j <90732785+cdsherrardjr@users.noreply.github.com>
2024-04-03 05:14:29 -0500

    Paragraph text is Times New Roman, new artwork photos, new credits

--- src/components/_aboutme.js
+++ src/components/_aboutme.js
@@ -7,11 +7,11 @@ function AboutMe() {
         <p>
           Corey De&apos;Juan Sherrard Jr (born 1996, Houston, TX) engineers a developing system for composing songs and generating objects that 
           respond to the deficit of black radical propaganda within a world culture. He began producing music in 2013 and, through undergrad, 
-          exploring different mediums/materials for design and artmaking. He graduated from the <a href="https://www.uh.edu" target="_blank" rel="noreferrer"><b>University of Houston</b></a> with a BS in Digital Media 
-          and currently consults at the <a href="https://www.houstonisd.org/hspva" target="_blank" rel="noreferrer"><b>High School for Performing and Visual Arts</b></a> in 
-          and spins jazz at <a href="https://ktru.org/" target="_blank" rel="noreferrer"><b>KTRU-LP 96.1 FM</b></a>. Sherrard showed his first solo 
+          exploring different mediums/materials for design and artmaking. He graduated from the <a href="https://www.uh.edu" target="_blank" rel="noreferrer"><b>University of Houston</b></a> with a BS in Digital Media, 
+          consults at the <a href="https://www.houstonisd.org/hspva" target="_blank" rel="noreferrer"><b>High School for Performing and Visual Arts</b></a> and 
+          spins jazz at <a href="https://ktru.org/" target="_blank" rel="noreferrer"><b>KTRU-LP 96.1 FM</b></a>. Sherrard showed his first solo 
           exhibition <i>Songbook for Black Constellations</i> at <a href="https://www.sanman.company/" target="_blank" rel="noreferrer"><b>Sanman Studios</b></a> in 2023 and has since
-          shown at <a href="https://www.sawyeryards.com/" target="_blank" rel="noreferrer"><b>Sabine Street Studios</b></a>, <a href="https://www.houstonendowment.org/" target="_blank" rel="noreferrer"><b>Houston Endowment</b></a>, 
+          shown at <a href="https://www.sawyeryards.com/" target="_blank" rel="noreferrer"><b>Sabine Street Studios</b></a>, <a href="https://www.houstonendowment.org/" target="_blank" rel="noreferrer"><b>Houston Endowment</b></a>,
           and <a href="https://www.theccma.org/" target="_blank" rel="noreferrer"><b>Cleve Carney Museum of Art</b></a>. 
           He is a Jones Artist Awardee and an alumni of the <a href="https://sfpc.study/" target="_blank" rel="noreferrer"><b>School for Poetic Computation</b></a>.
         </p>
@@ -19,24 +19,24 @@ function AboutMe() {
         <h3>solo exhibitions</h3>
         <p>
           <b>Songbook for Black Constellations, for Trio</b><br/>
-          Jones Artist Award; Houston Endownment & Weingarten Art Group, Houston, Texas<br/>
+          Jones Artist Award; Houston Endownment & Weingarten Art Group<br/>
           July 18 - October 16, 2023
         </p>
         <p>
           <b>Songbook for Black Constellations</b><br/>
-          Sanman Studios, Houston, Texas; Sabine Street Studios, Houston, Texas (Curated by William Baldwin)<br/>
+          Sanman Studios; Sabine Street Studios (Curated by William Baldwin)<br/>
           January 7, 2023; May 27 - July 8, 2023
         </p>
         
         <h3>group exhibitions</h3>
         <p>
           <b>Crossroads: Eastward Imprints - Houston Artists in Austin</b><br/>
-          East Austin Studio Tours, Austin, Texas<br/>
+          East Austin Studio Tours<br/>
           November 4 - November 19, 2023
         </p>
         <p>
           <b>Once: 2023 Emerging Artist Exhibition</b><br/>
-          Cleve Carney Museum of Art, Glen Ellyn, Illinois<br/>
+          Cleve Carney Museum of Art<br/>
           October 7, 2023 - January 4, 2024
         </p>
         <p>
--- src/components/_art.js
+++ src/components/_art.js
@@ -3,7 +3,6 @@ function Art() {
     <div id="art" className="component">
       <h1>works</h1>
       <h2>selected pieces from interdisciplinary practice</h2>
-      <p>* <i>[photo taken by <a href="https://www.instagram.com/visibleinlight/?hl=en">Alex Barber</a>]</i> </p>
       
       <div>
         <img src="HEI_230728_07.jpg" alt="Songbook for Black Constellations, for Trio" /><br/>
@@ -15,24 +14,20 @@ function Art() {
         <img src="030-AR5_7563.jpg" alt="Heirloom I, for Jazz" />
         <h3>Heirloom I, for Jazz</h3>
         <h4><i>2023</i>, cassette</h4>
-        <p>
-          SIDE A: HEIRLOOM I, FOR JAZZ [MIX]<br/>
-          SIDE B: "HEIRLOOM I + TALKING TO SELF" [SOLO ALTO SAXOPHONE IMPROVISATION]
-        </p>
 
         <img src="015-5DSR0426.jpg" alt="7 Trains to Mecca" />
         <h3>7 Trains to Mecca</h3>
         <h4><i>2023</i>, acrylic on wood panel, tuning pegs, and piano string*</h4>
 
-        <img src="011-5DSR0419.jpg" alt="Constellation for Black Masses in Amerikkka" />
+        <img src="Corey_Sherrard_7.jpg" alt="Constellation for Black Masses in Amerikkka" />
         <h3>Constellation for Black Masses in Amerikkka</h3>
         <h4><i>2022</i>, acrylic, ink, pencil on wood panel*</h4>
 
-        <img src="012-5DSR0420.jpg" alt="Constellation for Black Meccas in Amerikkka[?]" />
+        <img src="constellation for black meccas in amerikkka [2022] [cropped].jpg" alt="Constellation for Black Meccas in Amerikkka[?]" />
         <h3>Constellation for Black Meccas in Amerikkka[?] <span className="sold">SOLD</span></h3>
         <h4><i>2022</i>, acrylic, ink, pencil on wood panel*</h4>
 
-        <img src="013-5DSR0422.jpg" alt="Theme I, for Negro League Baseball" />
+        <img src="theme i, for negro league baseball [2022] [cropped].jpg" alt="Theme I, for Negro League Baseball" />
         <h3>Theme I, for Negro League Baseball</h3>
         <h4><i>2022</i>, acrylic, ink, pencil on wood panel*</h4>
 
@@ -40,10 +35,16 @@ function Art() {
         <h3>Black Education 42/50, pg. 1 + 2</h3>
         <h4><i>2022</i>, acrylic, ink, pencil on wood panel[s]*</h4>
 
-        <img src="004-5DSR0409.jpg" alt="US Primer" />
+        <img src="Corey_Sherrard_5.jpg" alt="US Primer" />
         <h3>US Primer</h3>
         <h4><i>2022</i>, acrylic, ink, pencil on wood panel*</h4>
       </div>
+      <h3>credits</h3>
+      <p>
+        The photos of <i>7 Trains to Meccas</i>, <i>Constellation for Black Masses in Amerikkka</i>, <i>Constellation for Black Meccas in Amerikkka[?]</i>
+        , <i>Theme I, for Negro League Baseball</i>, <i>Black Education 42/50, pg. 1 + 2</i>, and <i>US Primer</i> were taken by <a href="https://www.instagram.com/visibleinlight/?hl=en"><b>Alex Barber</b></a>.
+        The photos of <i>Songbook for Black Constellations, for Trio</i> were taken by <a href="https://www.lawrenceelizabeth.com/" target="_blank" rel="noreferrer"><b>Lawrence Elizabeth Knox</b></a>.
+      </p>
     </div>
   )
 }

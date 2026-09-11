651dcd8c105c3bfce51b695501ac767108dc28e2
cds-j <90732785+cdsherrardjr@users.noreply.github.com>
2026-09-11 14:57:31 -0500

    Font is Times New Roman, updated about me, updated updates

--- src/components/_aboutme.js
+++ src/components/_aboutme.js
@@ -1,69 +1,18 @@
 function AboutMe() {
   return (
     <div id="#about-me" className="component">
-      <h1>About Me</h1>
-      <h2>Corey De&apos;Juan Sherrard Jr</h2>
+      <h1>about me</h1>
       <div>
         <p>
-          Corey De&apos;Juan Sherrard Jr (born 1996, Houston, TX) engineers a developing system for composing songs and generating objects that 
-          respond to the deficit of black radical propaganda within a world culture. He began producing music in 2013 and, through undergrad, 
-          exploring different mediums/materials for design and artmaking. He graduated from the <a href="https://www.uh.edu" target="_blank" rel="noreferrer"><b>University of Houston</b></a> with a BS in Digital Media, 
-          consults at the <a href="https://www.houstonisd.org/hspva" target="_blank" rel="noreferrer"><b>Kinder High School for Performing and Visual Arts</b></a> and 
-          spins jazz at <a href="https://ktru.org/" target="_blank" rel="noreferrer"><b>KTRU-LP 96.1 FM</b></a> for his weekly show <b>STEAM</b>. Sherrard showed his first solo 
-          exhibition <i>Songbook for Black Constellations</i> at <a href="https://www.sanman.company/" target="_blank" rel="noreferrer"><b>Sanman Studios</b></a> in 2023 and has since
-          shown at <a href="https://www.sawyeryards.com/" target="_blank" rel="noreferrer"><b>Sabine Street Studios</b></a>, <a href="https://www.houstonendowment.org/" target="_blank" rel="noreferrer"><b>Houston Endowment</b></a>, <a href="https://www.theccma.org/" target="_blank" rel="noreferrer"><b>Cleve Carney Museum of Art</b></a>,
+          Corey Sherrard Jr (b. 1996) engineers a developing system for composing songs and generating objects that 
+          respond to the deficit of black radical propaganda within a world culture. He graduated from the <a href="https://www.uh.edu" target="_blank" rel="noreferrer"><b>University of Houston</b></a> with a BS in Digital Media. 
+          Sherrard has exhibited at <a href="https://www.sanman.company/" target="_blank" rel="noreferrer"><b>Sanman Studios</b></a>, <a href="https://www.sawyeryards.com/" target="_blank" rel="noreferrer"><b>Sabine Street Studios</b></a>, <a href="https://www.houstonendowment.org/" target="_blank" rel="noreferrer"><b>Houston Endowment</b></a>, <a href="https://www.theccma.org/" target="_blank" rel="noreferrer"><b>Cleve Carney Museum of Art</b></a>,
           and the <a href="https://www.austintexas.gov/department/george-washington-carver-museum-cultural-and-genealogy-center" target="_blank" rel="noreferrer"><b>George Washington Carver Museum and Cultural Center</b></a>. 
-          He is a Jones Artist Awardee and an alumni of the <a href="https://sfpc.study/" target="_blank" rel="noreferrer"><b>School for Poetic Computation</b></a>.
-        </p>
-
-        <h3 className="exhibitions-header">solo exhibitions</h3>
-        <p>
-          <b>Songbook for Black Constellations, for Trio</b><br/>
-          Jones Artist Award; Houston Endownment & Weingarten Art Group<br/>
-          July 18 - October 16, 2023
+          He is a <a href="https://houstonendowment.org/programs/songbook-for-black-constellations" target="_blank" rel="noreferrer"><b>2023 Jones Artist Awardee</b></a> and an alumni of the <a href="https://sfpc.study/" target="_blank" rel="noreferrer"><b>School for Poetic Computation</b></a>.
+          Under the pseudonym <a href="https://soundcloud.com/yaaje" target="_blank" rel="noreferrer"><b>Yaajé</b></a>, he releases music solo and collaboratively with his band <b>Essex Moor</b>.
         </p>
         <p>
-          <b>Songbook for Black Constellations</b><br/>
-          Sanman Studios; Sabine Street Studios<br/>
-          January 7, 2023; May 27 - July 8, 2023
-        </p>
-        
-        <h3 className="exhibitions-header">group exhibitions</h3>
-        <p>
-          <b>The Last Sky: Memory Drift (Part II)</b><br/>
-          Sabine Street Studios<br/>
-          November 14, 2024 - January 11, 2025
-        </p>
-        <p>
-          <b>Hot Bod</b><br/>
-          Basket Books & Art, Houston, Texas<br/>
-          July 27 - August 17, 2024
-        </p>
-        <p>
-          <b>Small Black Museum Vol. 3</b><br/>
-          George Washington Carver Museum and Cultural Center<br/>
-          May 16 - October 12, 2024
-        </p>
-        <p>
-          <b>Crossroads: Eastward Imprints - Houston Artists in Austin</b><br/>
-          East Austin Studio Tours<br/>
-          November 4 - November 19, 2023
-        </p>
-        <p>
-          <b>Once: 2023 Emerging Artist Exhibition</b><br/>
-          Cleve Carney Museum of Art<br/>
-          October 7, 2023 - January 4, 2024
-        </p>
-
-        <h3 className="exhibitions-header">performances</h3>
-        <p>
-          <b>2024 Texas Biennial</b><br/>
-          <i>"Performance of a Songbook for Black Constellations"</i><br/>
-          Blaffer Art Museum<br/>
-          October 18, 2024
-        </p>
-        <p>
-          <a href="https://docs.google.com/document/d/1svVGmhKcmaLOwaf09L4IKyyQiTEcJPIx-UgmSgJcs1A/edit?usp=sharing" target="_blank" rel="noreferrer"><b>Resume</b></a>
+          <a href="https://docs.google.com/document/d/1svVGmhKcmaLOwaf09L4IKyyQiTEcJPIx-UgmSgJcs1A/edit?usp=sharing" target="_blank" rel="noreferrer"><b>CV</b></a>
         </p>
       </div>
     </div>
--- src/components/_header.js
+++ src/components/_header.js
@@ -6,9 +6,8 @@ function Nav() {
     <div>
       <nav id="navbar">
         <ul>
-          <li><Link to="/art">works</Link></li>
+          <li><Link to="/art">selected works</Link></li>
           <li><Link to="/design">design</Link></li>
-          <li><Link to="/opennotes">references + open notes</Link></li>
           <li><Link to="/about-me">about me</Link></li>
         </ul>
       </nav>
@@ -20,19 +19,17 @@ function Header() {
   return (
     <div>
       <header>
-        <h1>corey de'juan sherrard jr</h1>
-        <h2>artist + engineer</h2>
+        <h1>Corey Sherrard Jr</h1>
+        <h2>visual; sound; engineering</h2>
         <Nav/>
         <div id="update">
-          <h3>**UPDATE**</h3>
-          <hr/>
+          <hr/><h3>Exhibiting</h3><hr/>
           <h4>
-            <b>Sanman Studios</b> artist-in-residence<hr/>
-            <b>Public Index</b> exhibition @ <b>Project Row Houses</b><br/>October 18, 2025 - February 15, 2026
+            <a href="https://utvac.org/event/how-to-grow-the-cowpea" target="_blank" rel="noreferrer"><b>How To Grow The Cowpea: Survival Strategies From The US South</b></a> (w/ Saharan Dust Season Collective) @ Visual Arts Center at University of Texas, Austin from <i>September 4, 2026 - March 5, 2027</i>
           </h4>
         </div>
       </header>
-      
+
     </div>
   )
 }
@@ -45,5 +42,6 @@ function Header() {
 */
 // <li><Link to="/design">design</Link></li>
 // <li><Link to="/programming">programming</Link></li>
+// <li><Link to="/opennotes">references + open notes</Link></li>
 
 export default Header;
